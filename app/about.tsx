e6f908416b142638f4117488c6bc3b4376fbed43
MelodyDuplaix <melo.surseine@gmail.com>
2025-04-03 13:30:59 +0200

    chore: création d'une nouvelle app et ajout d'une page about

---
+++ app/about.tsx
@@ -0,0 +1,23 @@
+import {View, Text, StyleSheet} from "react-native";
+import React from "react";
+
+const AboutScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>about</Text>
+        </View>
+    )
+}
+export default AboutScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "#25292e",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        color: "#fff"
+    }
+});
